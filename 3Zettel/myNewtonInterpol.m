e26afa3e98d189e82101ea108278680c43e474da
Felix Geyer <felix.geyer@tu-dortmund.de>
2017-05-15 15:08:09 +0200

    ...

--- 3Zettel/myNewtonInterpol.m
+++ 3Zettel/myNewtonInterpol.m
@@ -1,8 +1,14 @@
 # Programm zur Interpolation mit dem Newton-Verfahren
-function [c] = myNewtonInterpol(x, f)
+function c = myNewtonInterpol(x, f)
 n = length(x);
 c(1) = f(1);
 for k=1:n-1;
 c(k+1) = (f(k+1)-c(k))/(x(k+1)-x(k));
 k = k +1;
 end
+#for j = 2 : n 
+#   for k = 1 : n - j
+#      c(k, j) = (c(k+1, j - 1) - c(k, j - 1))/(x(k+j) - x(k));
+#   end
+end
+	
--- 3Zettel/test.m
+++ 3Zettel/test.m
@@ -1,3 +1,4 @@
 x = [0, 1/12, 1/6];
 f = 5*sin(3*pi*x) + 36*x.^2;
-c = myNewtonInterpol(x, f)
+c = koeffizienten(f, x)
+#d = myNewtonInterpol(x, f)
