eb8fd9a4925ff13f5fe80be72bd80da859377e55
Rune <rune.dominik@udo.edu>
2017-05-18 15:34:13 +0200

    weg mit dem bums

--- 3Zettel/myNewtonInterpol.m
+++ 3Zettel/myNewtonInterpol.m
@@ -1,12 +1,13 @@
 # Programm zur Interpolation mit dem Newton-Verfahren. Dieses Programm bestimmt
-# die Koeffizienten des Newton-Interploationspolynom
+# die Koeffizienten des Newton-Interploationspolynom. Übergeben werden der
+# Funktion ein Stützstellenvektor x und die dazu gehörigen y-Werte f
 function [c] = myNewtonInterpol(x, f)
 n = length(x);
 B(:,1) = transpose(f);
   for k=2:n;
     for l=1:n-k+1;
       B(l,k) = (B(l+1,k-1) - B(l, k-1)) ./ (x(l+k-1) - x(l));
-    end  
+    end
   end
   c = B(1,:);
 end
