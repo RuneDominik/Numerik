957681c0515f3c9964926256c73b603b6100c60d
Rune <rune.dominik@udo.edu>
2017-05-18 14:05:41 +0200

    Knoten

---
+++ 3Zettel/aknoten.m
@@ -0,0 +1,7 @@
+function x = aknoten(n)
+x = 0:n
+for a=0:n
+  x(a+1) = -1+(2/n)*a;
+  a = a+1;
+endfor
+end
---
+++ 3Zettel/tknoten.m
@@ -0,0 +1,7 @@
+function x = tknoten(n)
+x = 0:n
+for a=0:n
+  x(a+1) = cos((2*a+1)*pi/(2*n+2));
+  a = a+1;
+endfor
+end
