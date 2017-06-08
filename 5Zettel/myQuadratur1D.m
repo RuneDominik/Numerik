44bacdf4fb901c27817fae3205cc8013518ee939
Rune <rune.dominik@udo.edu>
2017-06-08 10:52:50 +0200

    Ferig

--- 5Zettel/myQuadratur1D.m
+++ 5Zettel/myQuadratur1D.m
@@ -1,3 +1,15 @@
-function [c] = myQuadratur1D (f, w, x)
+function [c] = myQuadratur1D (f, w, x);
 
-end
+#Anpassen der Gewichte auf das implizit durch x übergebene Intervall
+  n = length(w);
+  a = x(1);
+  b = x(n);
+  ak = w.*(b-a);
+
+#Quadraturformel:
+  c = 0;
+  for i = 1:n
+    c = c + ak(i)*f(x(i));
+  endfor
+
+endfunction
---
+++ 5Zettel/myQuadratur1DTest.m
@@ -0,0 +1,25 @@
+#Definieren der zu Integrierenden Funktion
+function y = testfunction (x)
+  y = 1./x^2;
+endfunction
+
+#Bekannte Quadraturgewichte für n=1,2,3
+w1 = [1/2 , 1/2];
+w2 = [1/6 , 4/6, 1/6];
+w3 = [1/8 , 3/8, 3/8, 1/8];
+
+#Quadraturpunkte für n=1,2,3 auf (0.5, 1) nach der Newton-Cotes-Formel
+x1 = [1/2 , 1];
+x2 = [2/4 , 3/4 , 1];
+x3 = [1/2 , 2/3 , 5/6 , 1];
+
+#Function-Handle
+f = @testfunction;
+
+#Quadratur
+disp("Trapezregel (n=1):")
+I1 = myQuadratur1D(f, w1, x1)
+disp("Simpsonregel (n=2):")
+I2 = myQuadratur1D(f, w2, x2)
+disp("Newtons 3/8 Regel (n=3):")
+I3 = myQuadratur1D(f, w3, x3)
