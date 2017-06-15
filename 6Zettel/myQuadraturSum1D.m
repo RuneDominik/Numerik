68f6b42316f9452a00b2480d76541ff793b63e07
Felix Geyer <felix.geyer@tu-dortmund.de>
2017-06-15 16:58:40 +0200

    a

---
+++ 6Zettel/myQuadraturSum1D.m
@@ -0,0 +1,24 @@
+function [v] = myQuadraturSum1D(f, w, p, a, b, N)
+h = (b-a)/N;
+# Teilintervalle bestimmen
+for i = 1:N
+  ak(i) = a + (i-1)*h;
+  bk(i) = a + i*h; 
+  endfor
+# Anpassung der Stützstellen
+n = length(p);
+for i = 1:n
+  phi(i) = (bk(i) - ak(i))/2 *p(i) + (bk(i) + ak(i))/2;
+  endfor
+# Anpassung Gewichte
+for i = 1:n 
+  g(i) = (b-a)/N *w(i)
+  endfor
+ # Quadratur
+ c = 0; 
+for i = 1:N
+  for k = ak(i):bk(i)
+      v = v + g(k)*f(phi(k));
+    endfor
+  endfor
+endfunction
